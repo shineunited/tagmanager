9d38ebac05fda2cdb1574811b3be7322295c1a61
Robert Lasseter <rlasseter@shineunited.com>
2017-05-30 11:13:28 -0500

    fix gtm container test

--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -13,7 +13,7 @@ class ContainerTest extends DataLayerTest {
 		$container = new Container($id);
 
 		$expected =  <<<CONTAINER
-<!-- Google Tag Manager: Container -->
+<!-- Begin: GTM Container -->
 <noscript><iframe src="//www.googletagmanager.com/ns.html?id={$id}"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -21,7 +21,7 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','{$id}');</script>
-<!-- End Google Tag Manager: Container -->
+<!-- End: GTM Container -->
 CONTAINER;
 
 		$this->assertEquals($expected, $container->render());
